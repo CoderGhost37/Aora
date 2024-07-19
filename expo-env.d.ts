6c86d194cdf0b5cf1685c97e5cce65a31d5506df
CoderGhost37 <coderghost37@gmail.com>
2024-07-19 14:23:33 +0530

    feat: init

---
+++ expo-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="expo/types" />
+/// <reference types="nativewind/types" />
+/// <reference types="expo-router/types" />
+
+// NOTE: This file should not be edited and should be in your git ignore
